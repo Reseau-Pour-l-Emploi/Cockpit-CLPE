8cc8270f35b47998579f9dd86ec98fb55a5326c0
dgorce-dev <143207343+dgorce-dev@users.noreply.github.com>
2026-05-21 11:43:25 +0200

    Change base path in Vite configuration
    Updated base path for GitHub repository.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/clpe-dashboard/', // Adapter selon le nom du dépôt GitHub
+  base: '/Cockpit-CLPE/', // Adapter selon le nom du dépôt GitHub
 })
